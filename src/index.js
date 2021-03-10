27e521df617d14538e627b695fabe00dc10c6b3d
fabio.alves <fabio.alves@exablack.com>
2021-03-10 18:25:13 -0300

    Starting express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Be Honest")
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+// Create a new express app instance
+var app = express();
+app.get("/", function (req, res) {
+    res.send("Be Honest");
+});
+app.listen(3000, function () {
+    console.log("App is listening on port 3000!");
+});
