0c25120cd7f536d77a02ee20a4f8808f2cc07362
fabio.alves <fabio.alves@exablack.com>
2021-03-10 19:02:33 -0300

    Removing ts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-// Create a new express app instance
-var app = express();
-app.get("/", function (req, res) {
-    res.send("Be Honest");
-});
-app.listen(3000, function () {
-    console.log("App is listening on port 3000!");
-});
+const express = require("express");
+const app = express();
+
+
+
+app.get("/", (req,res) => {
+    res.send("Be honest")
+})
+
+
+
+app.listen(3000, () =>{
+    console.log("Being Honest at 3000")
+}
+)
